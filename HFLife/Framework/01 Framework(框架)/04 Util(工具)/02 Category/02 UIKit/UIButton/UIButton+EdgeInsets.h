3c501d29482f9f0f2d6c345f7fa46e4445ff48f0
zhengchao <zhengchaono1@gmail.com>
2019-05-09 15:13:33 +0800

    sxftouchButton 修正

--- HFLife/Framework/01 Framework(框架)/04 Util(工具)/02 Category/02 UIKit/UIButton/UIButton+EdgeInsets.h
+++ HFLife/Framework/01 Framework(框架)/04 Util(工具)/02 Category/02 UIKit/UIButton/UIButton+EdgeInsets.h
@@ -20,11 +20,12 @@ typedef NS_ENUM(NSInteger, ImagePositionType) {
  
  if (button.width小于imageView上image的width){图像会被压缩，文字不显示}
  
- if (button.width < imageView.width + label.width){图像正常显示，文字显示不全}
- 
+ if (button.width < imageView.width + label.width){图像正常显示，文字显示不全,这种情况下竖直排列时,要特别注意,已解决}
+ https://www.jianshu.com/p/f521505beed9
  if (button.width >＝ imageView.width + label.width){图像和文字都居中显示，imageView在左，label在右，中间没有空隙}
  */
 @interface UIButton (EdgeInsets)
+
 /**
  *  利用UIButton的titleEdgeInsets和imageEdgeInsets来实现图片和标题的自由排布
  *  注意：1.该方法需在设置图片和标题之后才调用;
@@ -43,16 +44,6 @@ typedef NS_ENUM(NSInteger, ImagePositionType) {
  @param margin 图片、文字离button边框的距离
  */
 - (void)setImagePosition:(ImagePositionType)postion WithMargin:(CGFloat )margin;
-
-
-
-/**
- 商城tabBar上的plusButton 图文排版
-
- @param postion 图片位置
- @param spacing 图文间距
- */
-- (void)tabBarItem_setImagePosition:(ImagePositionType)postion spacing:(CGFloat)spacing;
 
 
 @end
--- HFLife/Framework/01 Framework(框架)/04 Util(工具)/02 Category/02 UIKit/UIButton/UIButton+EdgeInsets.m
+++ HFLife/Framework/01 Framework(框架)/04 Util(工具)/02 Category/02 UIKit/UIButton/UIButton+EdgeInsets.m
@@ -8,17 +8,10 @@
 
 #import "UIButton+EdgeInsets.h"
 
-#if __IPHONE_OS_VERSION_MIN_REQUIRED >= 70000
-#define _SINGLELINE_TEXTSIZE(text, font) [text length] > 0 ? [text \
-sizeWithAttributes:@{NSFontAttributeName:font}] : CGSizeZero;
-#else
-#define _SINGLELINE_TEXTSIZE(text, font) [text length] > 0 ? [text sizeWithFont:font] : CGSizeZero;
-#endif
-
 @implementation UIButton (EdgeInsets)
 
-
 - (void)setImagePosition:(ImagePositionType)postion spacing:(CGFloat)spacing {
+    self.titleEdgeInsets = self.imageEdgeInsets = UIEdgeInsetsZero;
     CGFloat imgW = self.imageView.image.size.width;
     CGFloat imgH = self.imageView.image.size.height;
     CGSize origLabSize = self.titleLabel.bounds.size;
@@ -27,9 +20,6 @@ sizeWithAttributes:@{NSFontAttributeName:font}] : CGSizeZero;
     
     CGSize trueSize = [self.titleLabel sizeThatFits:CGSizeMake(MAXFLOAT, MAXFLOAT)];
     CGFloat trueLabW = trueSize.width;
-    
-//    self.imageView.backgroundColor = [UIColor yellowColor];
-//    self.titleLabel.backgroundColor = [UIColor redColor];
     
     //image中心移动的x距离
     CGFloat imageOffsetX = orgLabW/2 ;
@@ -74,37 +64,19 @@ sizeWithAttributes:@{NSFontAttributeName:font}] : CGSizeZero;
 
 /**根据图文距边框的距离 自动调整图文间距*/
 - (void)setImagePosition:(ImagePositionType)postion WithMargin:(CGFloat )margin {
-    CGFloat imageWith = self.imageView.image.size.width;
-    CGFloat labelWidth = [self.titleLabel sizeThatFits:CGSizeMake(MAXFLOAT, MAXFLOAT)].width;
-    CGFloat spacing = self.bounds.size.width - imageWith - labelWidth - 2*margin;
-    
-    [self setImagePosition:postion spacing:spacing];
-}
-
-- (void)tabBarItem_setImagePosition:(ImagePositionType)postion spacing:(CGFloat)spacing {
-    CGFloat bottomMargin = 0.f;
-    CGFloat imgW = self.imageView.image.size.width;
-    CGFloat imgH = self.imageView.image.size.height;//(4,10) 8,xr(11.5,13.5) plus,x,xs,xsmax,(11.3,13.3)
-    CGSize origLabSize = IS_IPHONE_5 ? CGSizeMake(4, 10) : CGSizeMake(11.4, 13.4);
-//    CGSize origLabSize = self.titleLabel.frame.size;
-    CGFloat orgLabW = origLabSize.width;
-    CGFloat orgLabH = origLabSize.height;
-
-    CGSize trueSize = [self.titleLabel sizeThatFits:CGSizeMake(MAXFLOAT, MAXFLOAT)];
-    CGFloat trueLabW = trueSize.width;
-    //image中心移动的x距离
-    CGFloat imageOffsetX = orgLabW/2 ;
-    //image中心移动的y距离
-    CGFloat imageOffsetY = imgH/2-self.bounds.size.height/2+orgLabH+spacing+bottomMargin;
-    //label左边缘移动的x距离
-    CGFloat labelOffsetX1 = imgW/2 - orgLabW/2 + trueLabW/2;
-    //label右边缘移动的x距离
-    CGFloat labelOffsetX2 = imgW/2 + orgLabW/2 - trueLabW/2;
-    //label中心移动的y距离
-    CGFloat labelOffsetY = orgLabH-self.bounds.size.height/2+bottomMargin;
-    
-    self.imageEdgeInsets = UIEdgeInsetsMake(-imageOffsetY-10, imageOffsetX, imageOffsetY, -imageOffsetX);
-    self.titleEdgeInsets = UIEdgeInsetsMake(-labelOffsetY, -labelOffsetX1, labelOffsetY, labelOffsetX2);
+    if (postion == ImagePositionTypeLeft || postion == ImagePositionTypeRight) {
+        CGFloat imageWith = self.imageView.image.size.width;
+        CGFloat labelWidth = [self.titleLabel sizeThatFits:CGSizeMake(MAXFLOAT, MAXFLOAT)].width;
+        CGFloat spacing = self.bounds.size.width - imageWith - labelWidth - 2*margin;
+        
+        [self setImagePosition:postion spacing:spacing];
+    }else {
+        CGFloat imageHeight = self.imageView.image.size.height;
+        CGFloat labelHeight = [self.titleLabel sizeThatFits:CGSizeMake(MAXFLOAT, MAXFLOAT)].height;
+        CGFloat spacing = self.bounds.size.height - imageHeight - labelHeight - 2*margin;
+        
+        [self setImagePosition:postion spacing:spacing];
+    }
 }
 
 @end
