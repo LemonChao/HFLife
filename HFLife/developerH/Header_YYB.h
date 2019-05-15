802815c229b8f0e9035c2a0ce78232b7554a4485
yangyabo_117 <1171727303@qq.com>
2019-05-15 09:04:07 +0800

    登录显示三方

--- HFLife/developerH/Header_YYB.h
+++ HFLife/developerH/Header_YYB.h
@@ -13,6 +13,8 @@
 #import "HXEasyCustomShareView.h"
 /** 弹窗 */
 #import "ZHB_HP_PreventWeChatPopout.h"
+/** 网络异常界面 */
+#import "UIView+alertView.h"
 
 /************************yourTools************************/
 
@@ -25,7 +27,10 @@
 #define GP_BASEURL  @"http://test.hfgld.net"
 
 static NSString *const yourTestUrl = @"w=index&t=index";
-static NSString *const yourTestUrl2 = @"w=user&t=get_invite_info";
+/** 分享数据 */
+static NSString *const kGet_invite_info = @"w=user&t=get_invite_info";
+/** 综合商家店铺列表 */
+static NSString *const kGet_general_shop_list = @"w=general_shop&t=get_general_shop_list";
 
 /************************subUrl************************/
 
