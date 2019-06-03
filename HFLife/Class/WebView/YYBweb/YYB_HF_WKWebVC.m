4f552e37dca9ecf6c5c547071680517a0ee6de63
yangyabo_117 <1171727303@qq.com>
2019-06-03 18:30:24 +0800

    no message

--- HFLife/Class/WebView/YYBweb/YYB_HF_WKWebVC.m
+++ HFLife/Class/WebView/YYBweb/YYB_HF_WKWebVC.m
@@ -229,6 +229,8 @@
     self.webView.scrollView.bounces = NO;
     self.webView.contentMode = UIViewContentModeScaleAspectFit;
     self.webView.scrollView.delegate = self;
+    self.webView.allowsBackForwardNavigationGestures = YES;//允许侧滑返回
+
     [self.view addSubview:self.webView];
     if (@available(iOS 11.0, *)) {
         if (self.isTop) {//不是从状态栏开始布局
