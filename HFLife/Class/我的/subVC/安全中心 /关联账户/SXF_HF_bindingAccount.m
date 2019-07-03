84731e8f24f1b4c21c073ff267a5d127d871d2ad
sxf <381240780@qq.com>
2019-07-03 12:20:28 +0800

    绑定解绑逻辑 优化

--- HFLife/Class/我的/subVC/安全中心 /关联账户/SXF_HF_bindingAccount.m
+++ HFLife/Class/我的/subVC/安全中心 /关联账户/SXF_HF_bindingAccount.m
@@ -91,8 +91,8 @@
 
 //支付宝
 - (IBAction)tapAlipyCell:(UITapGestureRecognizer *)sender {
-    
-    if ([userInfoModel sharedUser].alipay_unionid) {
+    NSLog(@"%@", [userInfoModel sharedUser].alipay_unionid);
+    if ([userInfoModel sharedUser].alipay_unionid != [NSNull class] && [userInfoModel sharedUser].alipay_unionid.length != 0) {
         //解绑
       [SXF_HF_AlertView showAlertType:AlertType_binding_alipay Complete:^(BOOL btnBype) {
             if (btnBype) {
@@ -103,7 +103,7 @@
                         [userInfoModel sharedUser].alipay_unionid = nil;
                         SXF_HF_AlertView *alert =  [SXF_HF_AlertView showAlertType:(AlertType_exchnageSuccess) Complete:nil];
                         alert.title = @"解绑成功";
-                        self.wechatStateLabel.text = @"未绑定";
+                        self.alipayStateLabel.text = @"未绑定";
                         
                     }else {
                         if (value && [value isKindOfClass:[NSDictionary class]]) {
@@ -128,7 +128,7 @@
             [networkingManagerTool requestToServerWithType:POST withSubUrl:kCenterAdress(kMobileBindAlipay) withParameters:dataDict withResultBlock:^(BOOL result, id value) {
                 [[WBPCreate sharedInstance]hideAnimated];
                 if (result) {
-                    self.wechatStateLabel.text = @"已绑定";
+                    self.alipayStateLabel.text = @"已绑定";
                     [userInfoModel getUserInfo];
                     SXF_HF_AlertView *alert =  [SXF_HF_AlertView showAlertType:(AlertType_exchnageSuccess) Complete:nil];
                     alert.title = @"绑定成功";
